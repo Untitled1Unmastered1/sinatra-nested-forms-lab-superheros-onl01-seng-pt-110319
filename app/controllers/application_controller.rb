fdb9551a067376f3de0a6462ca12b76e6cd41100
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-14 20:28:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     end 
     
     post '/super_hero' do 
-      binding.pry
+   
       @team = Team.new(params[:team])
       params[:team][:member].each do |deets|
         Member.new(deets)
