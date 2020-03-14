dece6854f3ff2f402a1231d26d90a3c62320ddaa
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-14 20:00:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do 
+      erb :super_hero
+    end 
+    
+    post '/super_hero' do 
+      @team = Team.new(params[:team])
+      params[:team][:member].each do |deets|
+        Member.new(deets)
 
-
-end
+      end 
+      @superheroes = Member.all 
+      
+      
+      erb :team 
+    end 
+  end 
